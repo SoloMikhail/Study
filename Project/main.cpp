01afcd4b8851e1929dfe6ca865a7111779da3fb6
ivanov-mk <ivanov-mk@PC02>
2023-08-02 12:17:57 +0600

    Добавьте файлы проекта.

---
+++ Project/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	constexpr int WORLD_LENGTH = 5;
+	cout << "Welcome to BULLS AND COWS, a fun word game. \n";
+	cout << "Can you guess the X letter isogram I'm thinking of?\n";
+	
+	return 0;
+} 
